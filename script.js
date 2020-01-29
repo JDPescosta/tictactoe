c00b8480bfb06fde2c7f45e499ca881d931d434f
JDPescosta <jamesdpescosta@gmail.com>
2020-01-28 17:18:58 -0800

    finished base game functionality

---
+++ script.js
@@ -0,0 +1,67 @@
+const gameController = (doc => {
+  // const player = () => {
+  //     //establish whether player is X / O
+  //     let _Xexists = false;
+  //     let playerId = _Xexists ? 'X' : 'O';
+  //     if (playerId === 'X') _Xexists = true;
+  //     return {playerId}
+  // };
+
+  //const players = [player(), player()];
+
+  let currentTurn = 1;
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const checkGameState = (doc) => {
+    //let currentBoard = doc.getElementsByClassName("game-sqaure");
+    //console.log(currentBoard);
+    for (let i = 0; i < winConditions.length; i++) {
+      if (
+        currentBoard[winConditions[i][0]].innerHTML &&
+        currentBoard[winConditions[i][1]].innerHTML &&
+        currentBoard[winConditions[i][2]].innerHTML
+      ) {
+        if (
+          currentBoard[winConditions[i][0]].innerHTML ==
+            currentBoard[winConditions[i][1]].innerHTML &&
+          currentBoard[winConditions[i][0]].innerHTML ==
+            currentBoard[winConditions[i][2]].innerHTML
+        ) {
+          gameOver(currentBoard[winConditions[i][0]].innerHTML);
+          break;
+        }
+      }
+    }
+    currentTurn++;
+  };
+
+  const gameOver = (gameResult) => {
+    console.log(`Game over: ${gameResult} wins`);
+  };
+
+  const gameBoard = (() => {
+    let boardContainer = doc.getElementById("board-container");
+    let board = [];
+    for (let i = 0; i < 9; i++) {
+      board[i] = doc.createElement("div");
+      board[i].classList.add("game-square");
+      board[i].addEventListener("click", e => {
+        e.target.textContent = currentTurn % 2 !== 0 ? "X" : "O";
+        checkGameState();
+        if (currentTurn > 9) gameOver();
+      });
+      boardContainer.appendChild(board[i]);
+    }
+  })(doc);
+  const currentBoard = doc.getElementsByClassName("game-square");
+  console.log(doc.getElementsByClassName("game-square"), currentBoard);
+})(document);
